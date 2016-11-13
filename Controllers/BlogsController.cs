08cc981584ba4bdcd88b6ffa3271f1feabb57974
cuno2712 <hoangvannhattan@gmail.com>
2016-11-13 20:47:38 +0700

    init net core 1.0 web

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private BloggingContext _context;
+
+        public BlogsController(BloggingContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Employees.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                
+                _context.Employees.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+
+    }
+}
